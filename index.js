d6d44c4dea200aad358d3df95e502dfdcfacc6a6
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-01 20:36:11 -0700

    Add joi validation

--- index.js
+++ index.js
@@ -27,14 +27,13 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
 var { database } = include('databaseConnection');
+const userCollection = database.db(mongodb_database).collection('users');
 var mongoStore = MongoStore.create({
-    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@cluster0.fuu9a.mongodb.net/sessions`,
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
     crypto: {
         secret: mongodb_session_secret
     }
 })
-
-const userCollection = database.db(mongodb_database).collection('users');
 
 
 app.use(express.static("public"));
@@ -52,7 +51,7 @@ app.get('/', (req, res) => {
 });
 
 app.get("/nosql-injection", async (req, res) => {
-    var username = req.query.username;
+    var username = req.query.user;
 
     if (!username) {
         res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
@@ -60,13 +59,17 @@ app.get("/nosql-injection", async (req, res) => {
 	}
 	console.log("user: "+username);
 
-    const schema = Joi.object({
-        username: Joi.string().alphanum().max(20).required();
-        password: Joi.string().min(6).max(20).required()
-    });
-	const validationResult = schema.validate(req.body);
+    const schema = Joi.string().max(20).required();
 
-    
+	const validationResult = schema.validate(req.body);
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
+        return;
+    }
+
+    const result = await userCollection.find({ username: username }).project({ username: 1, email: 1, password: 1, _id: 1 }).toArray();
+    res.send();
 });
 
 app.get('/signupFailure', (req, res) => {
@@ -101,17 +104,16 @@ app.get('/signupFailure', (req, res) => {
 });
 
 
-app.get("/signup", (req, res) => { 
+app.get("/signup",  (req, res) => { 
     res.sendFile(__dirname + "/pages/signup.html");
 });
 
-app.post('/signup', (req, res) => { 
+app.post('/signup', async (req, res) => { 
 
     var username = req.body.user_name;
     var email = req.body.email;
     var password = req.body.password;
-    var hashedPassword = bcrypt.hashSync(password, saltRounds);
-    console.log(hashedPassword);
+    console.log("inside signup");
 
     if (!username && !email && !password) {
         console.log("All fields are missing.");
@@ -142,15 +144,29 @@ app.post('/signup', (req, res) => {
         res.redirect('/signupFailure?missing=7');
         return;
     } 
-    users.push({ username: email, password: password });
 
-    var usershtml = '';
-    // store the username in a session variable
-    for (i = 0; i < users.length; i++) {
-        usershtml += "<li>" + users[i].email + ": " + users[i].password + "</li>";
+    const schema = Joi.object({
+        username: Joi.string().alphanum().max(30).required(),
+        email: Joi.string().max(30).required(),
+        password: Joi.string().max(20).required(),
+    });
+
+    const validationResult = schema.validate({ username, email, password });
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect('/signup');
+        return;
     }
 
-    var html = "<ul>" + usershtml + "</ul>";
+    var hashedPassword = await bcrypt.hashSync(password, saltRounds);
+    console.log(hashedPassword);
+
+    await userCollection.insertOne({
+        username: username,
+        email: email,
+        password: hashedPassword,
+    });
+    console.log("user inserted");
 
     req.session.username = username;
     res.redirect('/members');
@@ -174,25 +190,44 @@ app.get("/login", (req, res) => {
     res.sendFile(__dirname + "/pages/login.html");
 });
 
-app.post('/login', (req, res) => { 
+app.post('/login', async (req, res) => { 
     console.log("inside login")
     var email = req.body.email;
     var password = req.body.password;
-
-        for (i = 0; i < users.length; i++) {
-        if (users[i].email == email) {
-            if (bcrypt.compareSync(password, users[i].password)) {
-                req.session.authenticated = true;
-                req.session.username = username;
-                req.session.cookie.maxAge = expireTime;
-
-                res.redirect('/loggedIn');
-                return;
-            }
-        }
-    }
     
-    res.redirect("/loginFailure");
+    const schema = Joi.object({
+        email: Joi.string().max(30).required(),
+        password: Joi.string().max(20).required(),
+    });
+
+    const validationResult = schema.validate({ email, password });
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/login");
+        return;
+    }
+
+    const user = await userCollection.findOne({ email: email });
+
+    if (!user) {
+        console.log("User not found");
+        res.redirect("/loginFailure");
+        return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+        req.session.authenticated = true;
+        req.session.username = user.username;
+        req.session.cookie.maxAge = expirationPeriod;
+        res.redirect('/members');
+        return;
+    } else {
+        console.log("Invalid email or password");
+        res.redirect("/loginFailure");
+        return;
+    }
 });
 
 app.get("/loggedIn", (res, req) => {
@@ -204,14 +239,14 @@ app.get("/loggedIn", (res, req) => {
 
 app.get("/logout", (req, res) => {
     req.session.destroy();
-    res.redirect('/');
+    res.redirect("/");
 });
 
 app.get("/members", (req, res) => { 
     console.log("inside members");
     const randomNumber = Math.floor(Math.random() * 3) + 1;
-    const image = "${randomNumber}.jpg";
-    const imagePath = "/images/image";
+    const image = `${randomNumber}.jpg`;
+    const imagePath = "/images/" + image;
 
     // retrieve the username from the session variable
     const username = req.session.username;
@@ -226,14 +261,14 @@ app.get("/members", (req, res) => {
 });
 
 
-// only for authenticated users
-const authenticatedUsersOnly = (req, res, next) => {
-  if (!req.session.GLOBAL_AUTHENTICATED) {
-    return res.status(401).json({ error: 'Not authenticated' });
-  }
-  next();
-};
-app.use(authenticatedUsersOnly);
+// // only for authenticated users
+// const authenticatedUsersOnly = (req, res, next) => {
+//   if (!req.session.GLOBAL_AUTHENTICATED) {
+//     return res.status(401).json({ error: 'Not authenticated' });
+//   }
+//   next();
+// };
+// app.use(authenticatedUsersOnly);
 
 app.get("*", (req, res) => { 
     res.status(404).sendFile(__dirname + "/pages/page-not-found.html");
@@ -243,6 +278,3 @@ app.get("*", (req, res) => {
 app.listen(port, () => {
     console.log(`Application is listening at http://localhost:${port}`);
 });
-
-
-    
